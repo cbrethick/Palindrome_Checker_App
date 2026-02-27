1d1302dcad8f133b4283a11ac898ae8658aa184c
RETHICK CB <rethickcb2007@gmail.com>
2026-02-27 09:02:16 +0530

    Implement palindrome checker in Palindrome_Checker_App

--- src/Palindrome_Checker_App.java
+++ src/Palindrome_Checker_App.java
@@ -1,5 +1,22 @@
 public class Palindrome_Checker_App {
     public static void main(String[] args) {
-        System.out.println("RCB");
+
+
+        String word = "madam";
+
+
+        String reverseWord = "";
+
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reverseWord = reverseWord + word.charAt(i);
+        }
+
+
+        if (word.equals(reverseWord)) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
     }
 }
