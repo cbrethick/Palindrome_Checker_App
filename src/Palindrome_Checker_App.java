94ba45417d57d4294772411e282a0662b7fcb522
rethickcb <rethickcb2007@gmail.com>
2026-02-27 09:22:31 +0530

    UC5 rcb5

--- src/Palindrome_Checker_App.java
+++ src/Palindrome_Checker_App.java
@@ -2,25 +2,30 @@ public class Palindrome_Checker_App {
 
     public static void main(String[] args) {
 
+
         String word = "madam";
 
-        char[] charArray = word.toCharArray();
 
-        int start = 0;
-        int end = charArray.length - 1;
+        Stack<Character> stack = new Stack<Character>();
+
+
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
+        }
 
         boolean isPalindrome = true;
 
-        while (start < end) {
 
-            if (charArray[start] != charArray[end]) {
+        for (int i = 0; i < word.length(); i++) {
+
+            char poppedChar = stack.pop();
+
+            if (word.charAt(i) != poppedChar) {
                 isPalindrome = false;
                 break;
             }
-
-            start++;
-            end--;
         }
+
 
         if (isPalindrome) {
             System.out.println(word + " is a Palindrome.");
