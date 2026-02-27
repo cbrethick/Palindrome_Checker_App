a1736f851d9b7f4edc140dd1b8a0eb8a42586d4d
rethickcb <rethickcb2007@gmail.com>
2026-02-27 09:29:10 +0530

    UC7 rcb7

--- src/Palindrome_Checker_App.java
+++ src/Palindrome_Checker_App.java
@@ -1,6 +1,5 @@
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.Stack;
+import java.util.Deque;
+import java.util.ArrayDeque;
 public class Palindrome_Checker_App {
 
     public static void main(String[] args) {
@@ -8,28 +7,23 @@ public class Palindrome_Checker_App {
         // Original String
         String word = "madam";
 
-        // Create Queue (FIFO)
-        Queue<Character> queue = new LinkedList<Character>();
+        // Create Deque
+        Deque<Character> deque = new ArrayDeque<Character>();
 
-        // Create Stack (LIFO)
-        Stack<Character> stack = new Stack<Character>();
-
-        // Insert characters into both Queue and Stack
+        // Insert characters into deque
         for (int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
-            queue.add(ch);   // Enqueue
-            stack.push(ch);  // Push
+            deque.addLast(word.charAt(i));  // Insert at rear
         }
 
         boolean isPalindrome = true;
 
-        // Compare dequeue and pop results
-        for (int i = 0; i < word.length(); i++) {
+        // Compare front and rear
+        while (deque.size() > 1) {
 
-            char fromQueue = queue.remove(); // Dequeue (FIFO)
-            char fromStack = stack.pop();    // Pop (LIFO)
+            char front = deque.removeFirst();  // Remove from front
+            char rear  = deque.removeLast();   // Remove from rear
 
-            if (fromQueue != fromStack) {
+            if (front != rear) {
                 isPalindrome = false;
                 break;
             }
